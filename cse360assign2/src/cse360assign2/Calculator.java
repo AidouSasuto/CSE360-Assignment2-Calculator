f3a12d8aee582613389cb98b055de578b27f3185
Zichang Wang <zichang.wang@asu.edu>
2019-02-23 21:54:05 -0700

    filled some blanks

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -9,27 +9,32 @@ private int total;
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	public void divide (int value) {
-		
+		total = total / value;
 	}
 	
 	public String getHistory () {
 		return "";
+	}
+	
+	public static void main(String []args)
+	{
+		System.out.println("Hello");
 	}
 	
 }//end of class
