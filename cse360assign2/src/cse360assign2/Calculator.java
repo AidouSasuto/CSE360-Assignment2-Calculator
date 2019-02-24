60e654ccda426be4a57e16f51d3cae2b9ea63b42
zichang Wang <zichang.wang@asu.edu>
2019-02-24 11:51:21 -0700

    Final version of Assignment 2

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,11 +1,16 @@
 package cse360assign2;
 
+import java.util.Scanner;
+
 public class Calculator {
 	
 private int total;
+private String history;
+private String menu;
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	public int getTotal () {
@@ -14,27 +19,85 @@ private int total;
 	
 	public void add (int value) {
 		total = total + value;
+		history = history + " + " + value;
 	}
 	
 	public void subtract (int value) {
 		total = total - value;
+		history = history + " - " + value;
 	}
 	
 	public void multiply (int value) {
 		total = total * value;
+		history = history + " * " + value;
 	}
 	
 	public void divide (int value) {
 		total = total / value;
+		history = history + " / " + value;
 	}
 	
 	public String getHistory () {
-		return "";
+		return history;
+	}
+	
+	/*public String menu()
+	{
+		menu = "1. + for add \n2. - for subtraction \n3. *for mutiplication \n4. / for divide \n5. any other input keys for quit calculator and show result and calculation history";
+		return menu;
+	}*/
+	public void menu()
+	{
+		System.out.println("1. + for add \n2. - for subtraction \n3. *for mutiplication \n4. / for divide \n5. any other input keys for quit calculator and show result and calculation history");
+		System.out.print("\n\n\t Please choose your calculating method:  ");
 	}
 	
 	public static void main(String []args)
 	{
-		System.out.println("Hello");
+		Scanner in = new Scanner (System.in);
+		Calculator zichang = new Calculator();
+		int flag = 1;
+		char receive = ' ';
+		int value;
+		while(flag == 1)
+		{
+			zichang.menu();
+			receive = in.next().charAt(0);
+			switch(receive) 
+			{
+				case '+': 
+				System.out.print("\ninput your number :  ");
+				value = in.nextInt();
+				System.out.println();
+				zichang.add(value);
+				break;
+				
+				case '-': 
+				System.out.print("\ninput your number :  ");
+				value = in.nextInt();
+				System.out.println();
+				zichang.subtract(value);
+				break;
+				
+				case '*': 
+				System.out.print("\ninput your number :  ");
+				value = in.nextInt();
+				System.out.println();
+				zichang.multiply(value);
+				break;
+				
+				case '/': 
+				System.out.print("\ninput your number :  ");
+				value = in.nextInt();
+				System.out.println();
+				zichang.divide(value);
+				break;
+				
+				default: flag = 2;
+				break;
+			}//end of switch		
+		}//end of while
+		System.out.println("\n\nThe result is : " + zichang.getTotal() + "\nThe history is : \n" + zichang.getHistory());	
 	}
 	
 }//end of class
